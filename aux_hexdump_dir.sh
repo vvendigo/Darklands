4f4a6d683933053e7165748aef96a8f7b5158abb
Miroslav Kucera <vvendigo@seznam.cz>
2018-03-22 21:58:45 +0100

    .IMC documentation (first draft)

--- aux_hexdump_dir.sh
+++ aux_hexdump_dir.sh
@@ -3,17 +3,18 @@
 DIR=$1
 LEN=$2
 
-echo -n '             '
+echo -n '                     '
 HDR=''
-for I in `seq 0 100` ; do
-	HDR=`printf "%s %2d" "$HDR" $I`
+for I in `seq 0 99` ; do
+	HDR=`printf "%s %3d" "$HDR" $I`
 done
-echo "$HDR" | head -c $LEN
+echo -n "$HDR" | head -c $LEN
 echo
 
 for F in `ls $DIR/ -1` ; do
-	printf '%12s:' $F
-	hexdump -v -e '/1 " %02X"' $DIR/$F | head -c $LEN
+	printf '%20s:' $F
+	#hexdump -v -e '/1 " %02X"' $DIR/$F | head -c $LEN
+	hexdump -v -e '/1 " %3u"' $DIR/$F | head -c $LEN
 	echo
 done
 
