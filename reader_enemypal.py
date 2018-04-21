73d9f2bc6220c62ceff3c642a9a07650e1f57d15
Wendigo <wendigo@nb>
2018-04-21 11:39:10 +0200

    enemy pal reader added

---
+++ reader_enemypal.py
@@ -0,0 +1,34 @@
+# based upon Olemar's info
+
+def readData(dlPath):
+    fname = dlPath + '/enemypal.dat'
+    data = map(ord, open(fname).read())
+    dataLen = len(data)
+
+    pals = []
+    pos = 0
+    while pos < dataLen:
+        pal = {}
+        startIdx = data[pos]/3
+        pos += 1
+        for i in xrange(startIdx, startIdx+16):
+            pal[i] = (data[pos], data[pos+1], data[pos+2])
+            pos += 3
+        pal['extra'] = data[pos:pos+4]
+        pos += 4
+        pals.append(pal)
+    return pals
+
+
+# main ------------
+if __name__ == '__main__':
+    import sys
+    from utils import itemStr
+
+    dlPath = sys.argv[1] if len(sys.argv) > 1 else 'DL'
+
+    data = readData(dlPath)
+
+    for i, pal in enumerate(data):
+        print '%2d: %s'%(i, pal)#['extra'])
+
