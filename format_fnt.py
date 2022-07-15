da77288b073323944e833b20964e0329bd8189df
Miroslav Kucera <miroslav.kucera@firma.seznam.cz>
2022-07-15 12:21:04 +0200

    python3 fixes

--- format_fnt.py
+++ format_fnt.py
@@ -23,7 +23,7 @@ class Font:
         hdrpos = offset - 8
         self.start_char, self.end_char, self.bw = unpack('BBB', data[hdrpos:hdrpos+3]) # first char ASCII code, last char ASCII code, char img width in bytes
         # 0 unknown
-        self.height, = unpack('B', data[hdrpos+4]) # height in bytes
+        self.height, = unpack('B', data[hdrpos+4:hdrpos+5]) # height in bytes
         # 1 - char spacing in px
         # 1 - line spacing in px
         # 0 unknown
@@ -31,13 +31,13 @@ class Font:
         width_off = offset - char_cnt - 8 # char widths array offset
         for i, ch_width in enumerate(unpack('B'*char_cnt, data[width_off:width_off+char_cnt])):
             char = Char(ch_width)
-            for j in xrange(0, self.height):
+            for j in range(0, self.height):
                 x = 0
                 ln_data = [0]*ch_width
                 for k in range(0, self.bw):
                     B_pos = offset + (j * char_cnt + i) * self.bw + k
                     ln_B, = unpack('B', data[B_pos:B_pos+1])
-                    for bc in xrange(0, 8):
+                    for bc in range(0, 8):
                         if x >= ch_width: break
                         ln_data[x] = 1 if ln_B & 0x80 else 0
                         x += 1
@@ -97,9 +97,9 @@ def write_fonts(fname, fonts):
 def read_fonts(fname):
     ''' get font list from file in DL format
     '''
-    data = open(fname).read()
+    data = open(fname, 'rb').read()
     dataLen = len(data)
-    #print fname, dataLen, 'B'
+    #print(fname, dataLen, 'B')
     pos = 0
 
     cnt = unpack('H', data[pos:pos+2])[0] ; pos += 2
@@ -131,15 +131,15 @@ if __name__ == '__main__':
     fonts = readData(dlPath)
 
     for ext, fnts in fonts.iteritems():
-        print '------', ext
+        print('------', ext)
         for i, fnt in enumerate(fnts):
-            print "-----", i, 'h:', fnt.height, fnt.start_char, '-', fnt.end_char, len(fnt.chars), 'bw', fnt.bw
+            print("-----", i, 'h:', fnt.height, fnt.start_char, '-', fnt.end_char, len(fnt.chars), 'bw', fnt.bw)
             chC = fnt.start_char
             for ch in fnt.chars:
-                print 'ch: %d, w: %d'%(chC, ch.width)
+                print('ch: %d, w: %d'%(chC, ch.width))
                 chC += 1
                 for r in ch.lines:
-                    print ''.join([('.','#')[b] for b in r])
-                print
-            print
+                    print(''.join([('.','#')[b] for b in r]))
+                print()
+            print()
 
